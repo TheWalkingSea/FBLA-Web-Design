cfa52a07f167a43c33de8fe0febb53fba8c96aa3
IndianaJonesR <rohit07.naras@gmail.com>
2022-12-21 23:24:52 -0500

    started on infocards
    started an infocard component. I need to fix their size. IMAGE NOT WORKING

---
+++ src/routes/infocards.svelte
@@ -0,0 +1,24 @@
+<script>
+    import rewards from './lib/images/rewards.jpg';
+</script>
+
+<div class="card">
+    <img src="{rewards}" alt="rocket Launching">
+    <div class="box">
+      <h4><b>Rewards</b></h4>
+      <p>Learn more about our rewards program</p>
+    </div>
+  </div>
+
+<style>
+    .card {
+        box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+        transition: .3s;
+        border-radius: 10px;
+        width: 30%;
+        height: 500%;
+    }
+    .card:hover{
+        box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+    }
+</style>
