d1f518d875df462af88cb4af4161f05e01566a8f
Austin McCallister <91487740+TheWalkingSea@users.noreply.github.com>
2022-12-22 18:12:29 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,6 +11,17 @@
 <Navbar userid={0}/>
 
 
+<<<<<<< Updated upstream
+=======
+    <div class="is-flex is-flex-direction-column is-flex-grow-1">
+        <div class="is-flex-grow-2 content is-justify-content-center" id="JetRocketHook">
+            <p>Travel Faster than a<br><strong>Jet.</strong></p>
+            <p>Travel in a <strong>Rocket.</strong></p>
+        </div>
+        <div class="is-flex-grow-1"><Finder/></div>
+        
+    </div>
+>>>>>>> Stashed changes
 
 <div class="columns is-justify-content-space-around">
     <Infocards title={"Rewards"}> 
